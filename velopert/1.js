e2e1837817375d708be3c935b4be9240de9999d3
taehoonoh <dkrakcjstk14@gmail.com>
2020-01-27 18:48:13 +0900

    200127
    변수와상수,데이터타입,산술,대입연산자

---
+++ velopert/1.js
@@ -0,0 +1,61 @@
+// 변수와상수,데이터타입,산술,대입연산자
+
+
+/* 변수(let)
+바뀔수있는값,같은 이름으로 선언할수없다.
+
+let value = 1; 
+// console.log(value);
+
+// let value2 = 5;
+// console.log(value2);
+*/
+
+/* 상수(const)
+값이 고정적이다
+
+const a = "hello"; 
+const b = "javascript";
+*/
+
+/* 데이터타입
++ 숫자(number)
++ 문자(string)
++ 참거짓(Boolean)
+
+/* null(없다)과 undefiend(확인되지않았다.) 
+
+let friend = null;
+let criminal;
+
+console.log(criminal);
+*/
+
+
+/* 산술연산자
+
+let a = 1;
+console.log(a++);
+console.log(a);
+console.log(++a);
+
+console.log(a--);
+console.log(a);
+console.log(--a);
+console.log(++a);
+console.log(a)
+console.log(a++)
+console.log(a);
+console.log(++a);
+console.log(a--);
+console.log(a); */
+
+/* 대입연산자
+
+let a = 1;
+a += 3;
+a -= 3;
+a *= 15;
+a /=3;
+
+console.log(a); */
