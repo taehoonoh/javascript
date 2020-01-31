b2fb55f48177da59d5d925d4be86d96e503337fa
taehoonoh <dkrakcjstk14@gmail.com>
2020-01-31 22:27:18 +0900

    Update 1.js

--- velopert/1.js
+++ velopert/1.js
@@ -59,3 +59,68 @@ a *= 15;
 a /=3;
 
 console.log(a); */
+
+
+//논리 연산자
+
+// NOT AND OR순서로 우선 처리한다
+
+// NOT !
+//const a =!true;
+//console.log(a)
+//값을 반전시킨다
+
+// AND &&
+//const a = true && false;
+//두개다 트루일경우만 트루
+
+// OR ||
+//const a =false || true;
+//console.log(a);
+//둘중 하나가 트루면 트루
+
+// const value = !((true && false) || (true && false) || !false);
+
+// console.log(value);
+
+
+//비교 연산자
+// const a = null;
+// const b = undefined;
+// const equals = a === b;
+// // ==면 타입을 검사하지않는다 ===이 정확하다
+// console.log(equals);
+
+// const a = 'a';
+// const b = 'b';
+// const notEquals = a !== b;
+// // 3 글자로 분석
+// console.log(notEquals);
+
+
+// const a =10;
+// const b =15;
+// const c =15;
+
+// console.log(a < b);
+// console.log(b > a);
+// console.log(b >= a);
+// console.log(a <=c);
+// console.log(b < c);
+// =이 뒤에온다
+
+//문자열붙이기
+// const a = '안녕';
+// const b = '하세요';
+
+// console.log(a+b);
+
+
+
+//--------------  삼항연산자
+
+// const array = [];
+// let text = array.length === 0 ?
+// '배열이 비어있습니다.' 
+// : '배열이 비어있지 않습니다.';
+// console.log(text);
